603f5efa13c59f60659045ac0ea2186db32efc79
oskrgrande <36568234+oskrgrande@users.noreply.github.com>
2018-04-15 15:44:15 -0600

    finally push

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+process.env["NTBA_FIX_319"] = 1;
 const TelegramBot = require('node-telegram-bot-api');
+var serialport = require("serialport");
 
 const token = '556353153:AAG7-2yVT3zbdQqx_OfLgJcRv_Yt3SrJQIg'; //token de acceso del bot
-var idChar = ;
 
-var serialport = require("serialport");
+
 //Cambiar el numero o nombre del puerto usb
-var miSerial = new serialport("/dev/ttyUSB0", {
+var miSerial = new serialport("COM3", {
   baudRate: 9600,
   autoOpen: true
 });
+
 
 const bot = new TelegramBot(token, {
   polling: true,
@@ -18,22 +20,23 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log("El id es " + chatId)
   var respuesta = msg.text;
-  if (respuesta == "Mostrar datos") {
+
     console.log("calculando...");
     bot.sendMessage(chatId, 'Mostrando datos...');
-    miSerial.write("Mostrar");
-    bot.sendMessage(chatId, miSerial.imprimir());
-  }
+  //  miSerial.write("Mostrar");
+  // bot.sendMessage(chatId, ;
+
 });
 
-/*
+// IMPORTANTE CAMBIAR ID DE CHAT EN LA VARIABLE PARA PRUEBAS
+var Idchat = 534322797;
 miSerial.setEncoding('utf8');
 
-miSerial.on('data', function(imprimir) {
-  console.log('Data:', data);
-  if (data[0] != '') {
-    console.log("Enviando datos");
-    bot.sendMessage(chatId,"Datos: "  );
-  }*/
+miSerial.on('data', function(data) {
+  console.log(data);
+  bot.sendMessage(Idchat,data);
+  if (data[0] == 'H') {
+    console.log("Boton precionado");
 
+  }
 });
