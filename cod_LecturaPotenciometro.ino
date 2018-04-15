995f1ec0636edd38856ec74af654a37dcbcb08e8
oskrgrande <36568234+oskrgrande@users.noreply.github.com>
2018-04-14 21:55:02 -0600

    add vector donde almacena numeros

--- cod_LecturaPotenciometro.ino
+++ cod_LecturaPotenciometro.ino
@@ -2,6 +2,10 @@
 long valor; //esta variable almacena el valor del potenciometro
 int numero; // esta avriable la cuparemos para definir el numero segun el rango en el que nos encontremos
 
+
+// variables para capturar valor mayor y menor
+int Lista[11];
+
 void setup() {
   // put your setup code here, to run once:
  // inicializamos la comunicacion serial
@@ -28,68 +32,79 @@ if(valor >= 0 && valor <= 93.09)
   numero == 0;
   Serial.print("El numero es = ");
   Serial.println(numero);
+  Lista[0]=0;
 }
 if(valor >= 93.1 && valor <= 186.19)
 {
   numero == 1;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[1]= numero;
 }
 if(valor >= 186.20 && valor <= 279.3)
 {
   numero == 2;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[2]=numero;
 }
 if(valor >= 279.4 && valor <= 372.5)
 {
   numero == 3;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[3]=numero;
 }
 if(valor >= 372.51 && valor <= 465.6)
 {
   numero == 4;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[4]=numero;
 }
 if(valor >= 465.61 && valor <= 558.8)
 {
   numero == 5;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[5]=numero;
 }
 if(valor >= 558.9 && valor <= 651.8)
 {
   numero == 6;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[6]=numero;
 }
 if(valor >= 651.9 && valor <= 744.9)
 {
   numero == 7;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[7]=numero;
 }
 if(valor >= 744.81 && valor <= 838.0)
 {
   numero == 8;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[8]=numero;
 }
 if(valor >= 838.1 && valor <= 931.1)
 {
   numero == 9;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[9]=numero;
 }
 if(valor >= 931.2 && valor <= 1023)
 {
   numero == 10;
   Serial.print("El numero es = ");
   Serial.println(numero);
+    Lista[10]=numero;
 }
-
+ delay(1000);
 // ahora vamos a capturar numeros: mayor, menor y actual
 
 
